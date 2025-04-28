9130f374f3ea744e62f0ac7dcd347a1118252529
Tagir Soltanakhmedov <s89926@bht-berlin.de>
2025-04-28 19:11:28 +0200

    server.js update landing.html

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-// Statische Dateien bereitstellen (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Root-Route: Landing Page (landing.html)
+// Root-Route: Landing Page zuerst definieren
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'landing.html'));
 });
 
-// Server starten
+// Danach statische Dateien
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}`);
 });
